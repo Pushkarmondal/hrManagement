4b6642cb1bcd446d4ba3845ad651a426dbbe4230
Pushkar Mondal <nishitm060@gmail.com>
2025-12-26 21:02:46 +0530

    Small changes

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,12 @@ import express from "express";
 import { healthCheck } from "./src/controllers/health.controller";
 
 const router = express.Router();
+const app = express();
 
 router.get("/health", healthCheck);
 
-export default router;
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
+
+export default app;
